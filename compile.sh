b95f88480218b224d0f50ce69c2dc565a4895674
Congyuwang <116020237@link.cuhk.edu.cn>
2020-04-14 03:10:05 +0800

    update shell files

--- compile.sh
+++ compile.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-javac -d /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/src/Field.java /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/src/FieldState.java /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/src/Game.java /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/src/Main.java /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/src/State.java /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/src/TicTacToeField.java /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/src/TicTacToeGame.java
+
+javac -d /Users/congyuwang/IdeaProjects/TicTacToe/out /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/Field.java /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/FieldState.java /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/Game.java /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/Main.java /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/State.java /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/TicTacToeField.java /Users/congyuwang/IdeaProjects/TicTacToe/tictactoe/TicTacToeGame.java
+
+cp -a /Users/congyuwang/IdeaProjects/TicTacToe/resource /Users/congyuwang/IdeaProjects/TicTacToe/out/
--- tictoctoe.sh
+++ tictoctoe.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /Users/congyuwang/IdeaProjects/TicTacToe
+cd /Users/congyuwang/IdeaProjects/TicTacToe/out
 java tictactoe.Main
